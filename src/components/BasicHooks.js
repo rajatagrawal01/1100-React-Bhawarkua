02f89160d74fe3065dab1a2663858af7c91a9f3f
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-01-01 11:54:18 +0530

    toggle button

--- src/components/BasicHooks.js
+++ src/components/BasicHooks.js
@@ -1,13 +1,33 @@
+// Single side working========================================
+
+// import {useState} from 'react'
+
+// export default function BasicHooks() {
+//     const [headingText,setHeadingText]=useState("Guest");
+//     console.log("component loaded");
+//   return (
+//     <div>
+//       <h1>Hello,{headingText}</h1>
+//       <input />
+//       <button onClick={()=>setHeadingText("Rajat")}>Click Here to change</button>
+//     </div>
+//   )
+// }
+
+
+// Toggle side working========================================
+
 import {useState} from 'react'
+import on from '../assets/image.jpg'
+import off from '../assets/download.jpg'
 
 export default function BasicHooks() {
-    const [headingText,setHeadingText]=useState("Guest");
-    console.log("component loaded");
+    const [isClicked,setIsClicked]=useState(true);
   return (
     <div>
-      <h1>Hello,{headingText}</h1>
-      <input />
-      <button onClick={()=>setHeadingText("Rajat")}>Click Here to change</button>
+      <h1>Button is : {isClicked?"On":"Off"}</h1>
+      {/* <img src={isClicked?on:off}/> */}
+      <button onClick={()=>setIsClicked(!isClicked)}>Toggle</button>
     </div>
   )
 }
